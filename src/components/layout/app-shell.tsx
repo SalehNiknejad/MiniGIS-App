c11500c7d45f2643620c6db673f27f6bfb2d4c03
Saleh <seyed.saleh.niknejad@gmail.com>
2026-05-07 21:08:55 +0330

    feat: replace placeholder elements with AppSidebar, AppHeader, and MapView components in AppShell

--- src/components/layout/app-shell.tsx
+++ src/components/layout/app-shell.tsx
@@ -1,12 +1,15 @@
+import { AppHeader } from "./app-header"
+import { AppSidebar } from "./app-sidebar"
+import { MapView } from "./map-view"
+
 export function AppShell() {
   return (
     <div className="flex h-svh overflow-hidden bg-background text-foreground">
-      <div>sidebar</div>
+      <AppSidebar />
       <div className="flex min-w-0 flex-1 flex-col">
-        <div>header</div>
-
+        <AppHeader />
         <main className="relative flex-1 overflow-hidden">
-          <div>mapbox</div>
+          <MapView />
         </main>
       </div>
     </div>
