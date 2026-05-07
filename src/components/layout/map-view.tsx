7d373ddaa27d0578a5e8bd4941cdbe06496900fe
Saleh <seyed.saleh.niknejad@gmail.com>
2026-05-07 23:17:35 +0330

    refactor: remove unused import and logs

--- src/components/layout/map-view.tsx
+++ src/components/layout/map-view.tsx
@@ -1,4 +1,3 @@
-import { useTranslation } from "react-i18next"
 import Mapir from "mapir-react-component"
 
 export function MapView() {
@@ -13,8 +12,6 @@ export function MapView() {
       }
     },
   })
-  console.log(import.meta.env.VITE_MAPIR_API_KEY)
-  console.log(Map)
   return (
     <div className="relative h-full w-full bg-muted">
       <div className="absolute inset-0 flex items-center justify-center">
